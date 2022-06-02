a2a7b0171b74f98228b07b01c2f756624230d940
Vladimir Gorej <vladimir.gorej@gmail.com>
2022-06-02 22:51:19 +0200

    chore: use alias

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import SwaggerUI from 'swagger-ui/dist/swagger-ui-es-bundle';
+import SwaggerUI from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
 
 globalThis.swaggerUIInstance = SwaggerUI({
