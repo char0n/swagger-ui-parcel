5d943bb56354eef957a0cf43233b055111aca4f8
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-01-08 15:42:13 +0100

    chore: integrate swagger-ui with React@18 support (#4)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 const container = document.getElementById('swagger-ui');
+const root = createRoot(container);
 
-ReactDOM.render(<SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />, container);
+root.render(<SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />);
